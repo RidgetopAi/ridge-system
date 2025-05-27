d0a0b7296d54bf81b247c94f6bac6da492a85a23
RidgetopAi <brianj@ridgetopai.net>
2025-05-27 09:32:59 -0400

    Remove exposed API key

--- chat-ui/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://gahnpzedurpgilixtppr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhaG5wemVkdXJwZ2lsaXh0cHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMzgxOTEsImV4cCI6MjA2MzcxNDE5MX0.RaDL8FsdzpDWzIoNHssdYxGitKHRPMFY_wtjtcEMf0o'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
